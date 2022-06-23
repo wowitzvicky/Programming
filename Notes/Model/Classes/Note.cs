9a40fffe584c3d9f6503dd344843aa1e2e71df08
GameIh <dimakuksov311@gmail.com>
2022-06-23 10:35:53 +0700

    Адаптив

--- Notes/Model/Classes/Note.cs
+++ Notes/Model/Classes/Note.cs
@@ -10,34 +10,27 @@ namespace Notes.Model.Classes
 		/// <summary>
 		/// Количество уже созданных когда-либо заметок.
 		/// </summary>
-		public static int AllNotesCount = 0;
-
+		public static int AllNotesCount=0;
 		/// <summary>
 		/// Уникальный Id - номер заметки.
 		/// </summary>
 		private int _id { get; set; }
-
 		/// <summary>
 		/// Название заметки.
 		/// </summary>
-		public string NoteName { get; set; }
-
+		public string Name { get; set; }
 		/// <summary>
 		/// Текст заметки.
 		/// </summary>
-		public string NoteText { get; set; }
-
+		public string Text { get; set; }
 		/// <summary>
 		/// Категория заметки.
 		/// </summary>
-		public string NoteCategory { get; set; }
-		
-		
+		public string Category { get; set; }
 		/// <summary>
 		/// Дата создания заметки.
 		/// </summary>
-		readonly public DateTime  AddTime;
-
+	    public DateTime  AddTime;
 		/// <summary>
 		/// Возвращает либо увеличивает кол-во уже созданных заметок.
 	    // </summary>
@@ -52,7 +45,6 @@ namespace Notes.Model.Classes
 				AllNotesCount = value;
 			}
 		}
-
 		public int Id
 		{
 			get
@@ -64,50 +56,47 @@ namespace Notes.Model.Classes
 				_id = value;
 			}
 		}
-
 		/// <summary>
 		/// Возвращает или задает название заметки.
 		/// </summary>
-		public string Name
+		public string NotesName
 		{
 			get
 			{
-				return NoteName;
+				return Name;
 			}
 			private set
 			{
-				Validator.AssertValueInRange(NoteName);
-				NoteName = value;
+				Validator.AssertValueInRange(Name);
+				Name = value;
 			}
 		}
-
 		/// <summary>
 		/// Возвращает или задает текст заметки.
 		/// </summary>
-		public string Text
+		public string NotesText
 		{
 			get
 			{
-				return NoteText;
+				return Text;
 			}
 			private set
 			{
-				NoteText = value;
+				Text = value;
 			}
 		}
-
 		/// <summary>
 		/// Возвращает или задает категорию заметки.
 		/// </summary>
-		public string Category
+		public string NotesCategory
 		{
 			get
 			{
-				return NoteCategory;
+				return Category;
 			}
 			private set
 			{
-				NoteText = value;
+				Category = value;
 			}
 		}
 
@@ -126,9 +115,9 @@ namespace Notes.Model.Classes
 		{
 			AllNotesCount++;
 			Id = AllNotesCount;
-			NoteName = name;
-			NoteText = text;
-			NoteCategory = category;
+			Name = name;
+			Text = text;
+			Category = category;
 			AddTime = DateTime.Now;
 		}
 
@@ -141,9 +130,9 @@ namespace Notes.Model.Classes
 		public Note(int id,string name, string text, string category)
 		{
 			Id = id;
-			NoteName = name;
-			NoteText = text;
-			NoteCategory = category;
+			Name = name;
+			Text = text;
+			Category = category;
 			AddTime = DateTime.Now;
 		}
 
